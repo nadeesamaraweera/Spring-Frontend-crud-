75a291fedfb673f7294197c8ff1c3b179a149997
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-08-07 10:55:51 +0530

    update

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,11 @@
 $('#savepost').click(function (){
 
-    console.log('event is triggered!')
+    // console.log('event is triggered!')
+
+    let postId =$('#post-id').val();
+    let postTitle =$('#post-title').val();
+    let postContent =$('#post-content').val();
+    console.log(postId,postTitle,postContent);
+
+
 })

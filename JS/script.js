a4416c9aacd946ce5b8cff80871d6d2d22184bec
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-08-07 11:16:45 +0530

    add the ajax

--- JS/script.js
+++ JS/script.js
@@ -6,6 +6,29 @@ $('#savepost').click(function (){
     let postTitle =$('#post-title').val();
     let postContent =$('#post-content').val();
     console.log(postId,postTitle,postContent);
+    $.ajax({
+        url:"http://localhost:8080/blog/savepost",
+        method:"POST",
+        contentType:"application/json",
+        "data":JSON.stringify({
+            "id":postId,
+            "title":postTitle,
+            "content":postContent
+        }),
+        success:function (result){
+           console.log(result)
+           alert("done")
+        },
+        error:function (error){
+            console.log(error)
+            alert("try again")
+
+        }
+    })
 
 
 })
+
+
+
+

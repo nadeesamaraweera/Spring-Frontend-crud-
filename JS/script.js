c5cea894aece0f1e5548e937ebe073e3e1f791ac
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-08-07 11:28:00 +0530

    add upadate post using ajax

--- JS/script.js
+++ JS/script.js
@@ -27,7 +27,35 @@ $('#savepost').click(function (){
     })
 
 
+
+
 })
+
+$('#updatepost').click(function () {
+    let postId = $('#post-id').val();
+    let postTitle = $('#post-title').val();
+    let postContent = $('#post-content').val();
+    console.log(postId, postTitle, postContent);
+
+    $.ajax({
+        url: "http://localhost:8080/blog/updatepost", // Update the endpoint URL
+        method: "PUT", //
+        contentType: "application/json",
+        data: JSON.stringify({
+            "id": postId,
+            "title": postTitle,
+            "content": postContent
+        }),
+        success: function (result) {
+            console.log(result);
+            alert("Post updated successfully!");
+        },
+        error: function (error) {
+            console.log(error);
+            alert("Update failed, please try again.");
+        }
+    });
+});
 
 
 
